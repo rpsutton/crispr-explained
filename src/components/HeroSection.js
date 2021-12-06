27230074cf7f92fa2bebacb939550b0eb6f86d15
rpsutton <57606697+rpsutton@users.noreply.github.com>
2021-12-06 15:51:39 -0500

    fixing error

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,8 +2,6 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-import { LinkContainer } from "react-router-bootstrap";
 import Image from "react-bootstrap/Image";
 import Section from "./Section";
 import SectionHeader from "./SectionHeader";
